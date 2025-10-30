d9ced362bb20abd10d63ea608c2f6ce933158c88
Anton L. <ohoku@yahoo.com>
2025-10-30 18:05:55 +0000

    Add password length input & fix styles

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let passwordLengthEl = document.querySelector('input[type="number"]')
 let btnEl = document.getElementById("btn-el")
 let password1El = document.getElementById("password1-el")
 let password2El = document.getElementById("password2-el")
 let theme = "dark"
 
-
 btnEl.addEventListener("click", function() {
-    password1El.textContent = generateRandomPassword()
-    password2El.textContent = generateRandomPassword()
+    let passwordLength = parseInt(getPasswordLength())
+    passwordLengthEl.value = ""
+    password1El.textContent = generateRandomPassword(passwordLength)
+    password2El.textContent = generateRandomPassword(passwordLength)
 })
 
 // password1El.addEventListener("click", function() {
@@ -30,9 +32,16 @@ btnThemeToogleEl.addEventListener("click", function() {
     }
 })
 
-function generateRandomPassword() {
+function getPasswordLength() {
+    if (passwordLengthEl.value === "" || (passwordLengthEl.value > 20 || passwordLengthEl.value < 5)) {
+        return 15
+    } else {
+        return passwordLengthEl.value
+    }
+}
+
+function generateRandomPassword(passwordLength) {
     let randomPassword = ""
-    let passwordLength = 15
     for (let i = 0; i <= passwordLength; i++) {
         randomPassword += getRandomCharacter()
     }
